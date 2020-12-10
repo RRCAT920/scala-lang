fab27e4e5f17313e56d83bbfff3084d76dc97232
胡子豪 <wuqieor@outlook.com>
2020-12-10 19:06:48 +0800

    [从Java到Scala][Scala和Java的差异]：可有可无的分号

---
+++ src/fromjavatoscala/OptionalSemicolon.scala
@@ -0,0 +1,22 @@
+package fromjavatoscala
+
+import java.util
+
+/**
+ * @author huzihao
+ * @since 2020/12/10 19:02
+ */
+object OptionalSemicolon extends App {
+  val list1 = new util.ArrayList[Int];
+  {
+    println("创建List1")
+  }
+
+  // 匿名内部类
+  val list2 = new util.ArrayList[Int] {
+    println("创建List2")
+  }
+
+  println(list1.getClass)
+  println(list2.getClass)
+}

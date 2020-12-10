e164552d56668b5ad63337083e63e93cb95bacf8
胡子豪 <wuqieor@outlook.com>
2020-12-10 16:58:19 +0800

    [从Java到Scala]字符串插值：插值器的使用

---
+++ src/fromjavatoscala/StringInterpolation.scala
@@ -0,0 +1,12 @@
+package fromjavatoscala
+
+/**
+ * @author huzihao
+ * @since 2020/12/10 16:45
+ */
+object StringInterpolation extends App {
+  val product = "ticket"
+  val price = 25.12
+  val discount = 10
+  println(s"On $product $discount% saves $$${price * discount / 100.00}")
+}

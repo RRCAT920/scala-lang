1dd8ff30086986c0d93f53d4d0cef3fc11a7bd48
胡子豪 <wuqieor@outlook.com>
2020-12-10 19:15:14 +0800

    [从Java到Scala][Scala和Java的差异]：避免显示return

---
+++ src/fromjavatoscala/AvoidExplicitReturn.scala
@@ -0,0 +1,17 @@
+package fromjavatoscala
+
+/**
+ * @author huzihao
+ * @since 2020/12/10 19:07
+ */
+object AvoidExplicitReturn extends App {
+  def check1 = true
+
+  def check2: Boolean = return true
+
+  def check3: Boolean = true
+
+  println(check1)
+  println(check2)
+  println(check3)
+}

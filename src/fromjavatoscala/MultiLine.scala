600a9ee7797e572164e09612c957b7efaecc1417
胡子豪 <wuqieor@outlook.com>
2020-12-10 16:39:06 +0800

    [从Java到Scala]字符串和多行原始字符：重点是RichString

---
+++ src/fromjavatoscala/MultiLine.scala
@@ -0,0 +1,16 @@
+package fromjavatoscala
+
+/**
+ * @author huzihao
+ * @since 2020/12/10 16:35
+ */
+object MultiLine extends App {
+  val str =
+    """hello
+      |sjklfsd
+      |sfjkls
+      |sjdkf
+      |sjdfksdjfldks
+      |""".stripMargin
+  println(str)
+}

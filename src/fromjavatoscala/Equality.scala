20ec6c4fa6c18306986ca20126a535d5c1eed2d4
胡子豪 <wuqieor@outlook.com>
2020-12-10 18:57:56 +0800

    [从Java到Scala][Scala和Java的差异]：Scala的==总是值比较

---
+++ src/fromjavatoscala/Equality.scala
@@ -0,0 +1,16 @@
+package fromjavatoscala
+
+/**
+ * @author huzihao
+ * @since 2020/12/10 18:55
+ */
+object Equality extends App {
+  val str1 = "hello"
+  val str2 = "hello"
+  val str3 = new String("hello")
+
+  println(str1 == str2)
+  println(str1 eq str2)
+  println(str1 == str3)
+  println(str1 eq str3)
+}
